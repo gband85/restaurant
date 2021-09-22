27e584b273600a9730a0c8a8b6d1ac69ae775118
Garrett Anderson <keaton85@runbox.com>
2021-09-22 18:46:35 -0500

    add event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import home from './home';
 
-home();
+window.onload(function() {
+    home();
+});
+document.querySelector("#home").addEventListener("click", function() {
+    home();
+});
+document.querySelector("#contact").addEventListener("click", function() {
+    home();
+});
+document.querySelector("#menu").addEventListener("click", function() {
+    home();
+});
+
