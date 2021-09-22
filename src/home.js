c84c2f51a4f2d5162094332528392ee0abf847d8
Garrett Anderson <keaton85@runbox.com>
2021-09-22 17:24:31 -0500

    create home structure

--- src/home.js
+++ src/home.js
@@ -1,22 +1,47 @@
 const home = () => {
-     const content = document.querySelector("#content");
-     //create header
-    const header=document.createElement("header");
-    header.id="header";
+    const content = document.querySelector("#content");
+    //create header
+    const header = document.createElement("header");
+    header.id = "header";
     //navbar
-    const nav=document.createElement("nav");
-    nav.id="navbar";
-    header.appendChild(nav);    
-//main section
-const main = document.createElement("main");
-main.id="main";
-//footer
-const footer = document.createElement("footer");
-footer.id="footer";
-//add to content div
-content.appendChild(header);
-content.appendChild(main);
-content.appendChild(footer);
+    const nav = document.createElement("nav");
+    nav.id = "navbar";
+    const navList = document.createElement("ul");
+
+    const homeLink = document.createElement("li");
+    const homeLink_a = document.createElement("a");
+    homeLink_a.href = "#";
+    homeLink_a.textContent = "Home";
+    homeLink_a.classList.add("selected");
+    homeLink.appendChild(homeLink_a);
+
+    const contactLink = document.createElement("li");
+    const contactLink_a = document.createElement("a");
+    contactLink_a.href = "#";
+    contactLink_a.textContent = "Contact";
+    contactLink.appendChild(contactLink_a);
+
+    const menuLink = document.createElement("li");
+    const menuLink_a = document.createElement("a");
+    menuLink_a.href = "#";
+    menuLink_a.textContent = "Menu";
+    menuLink.appendChild(menuLink_a);
+
+    nav.appendChild(navList);
+    navList.appendChild(homeLink);
+    navList.appendChild(contactLink);
+    navList.appendChild(menuLink);
+    header.appendChild(nav);
+    //main section
+    const main = document.createElement("main");
+    main.id = "main";
+    //footer
+    const footer = document.createElement("footer");
+    footer.id = "footer";
+    //add to content div
+    content.appendChild(header);
+    content.appendChild(main);
+    content.appendChild(footer);
 
 }
 
