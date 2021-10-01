87840e8527701ec0ce83234bfd70a64e5f2f8e93
Garrett Anderson <keaton85@runbox.com>
2021-09-30 19:56:47 -0500

    add contact class

--- src/contactTab.js
+++ src/contactTab.js
@@ -14,16 +14,19 @@ const contactTab = () => {
     title.appendChild(titleText);
 
     const contact1=document.createElement("div");
+    contact1.className="contact";
     const contact1Text = document.createElement("p");
     contact1Text.innerHTML="Donald Westinghouse, Owner<br>Phone: 955-867-5309<br>dwestinghouse@restaurantmail.com"
     contact1.appendChild(contact1Text);   
     
     const contact2=document.createElement("div");
+    contact2.className="contact";
      const contact2Text = document.createElement("p");
     contact2Text.innerHTML="Mary Johnson, Manager<br>Phone: 955-867-5309<br>mjohnson@restaurantmail.com"
     contact2.appendChild(contact2Text);
 
     const contact3=document.createElement("div");
+    contact3.className="contact";
 const contact3Text = document.createElement("p");
     contact3Text.innerHTML="John Abel, Assistant Manager<br>Phone: 955-867-5309<br>jabel@restaurantmail.com"
     contact3.appendChild(contact3Text);
