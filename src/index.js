7fa9e594a5c6d16f2234fe8c14bb449145e524f0
gband85 <garrett@gband.net>
2022-03-25 15:02:03 -0500

    disable menu button

--- src/index.js
+++ src/index.js
@@ -15,33 +15,33 @@ import './style.css';
 // }
 // const navbar=document.getElementById("navbar");
 // const navList=document.getElementById("nav-list");
-navList.style.display="none";
-hamburger.addEventListener("click",function() {
-   if (navList.style.display=="none") {
-       navList.style.display="flex";
-   }
-   else {
-       navList.style.display="none";
-   }
-   })
+//  navList.style.display="none";
+// hamburger.addEventListener("click",function() {
+//    if (navList.style.display=="none") {
+//        navList.style.display="flex";
+//    }
+//    else {
+//        navList.style.display="none";
+//    }
+//    })
 
 document.querySelector("#home").addEventListener("click", function() {
    homeTab();
-   if (hamburger.style.display="block") {
-       navList.style.display="none"
-   }
+//    if (hamburger.style.display="block") {
+//        navList.style.display="none"
+//    }
 });
 document.querySelector("#menu").addEventListener("click", function() {
     menuTab();
-    if (hamburger.style.display="block") {
-        navList.style.display="none"
-    }
+    // if (hamburger.style.display="block") {
+    //     navList.style.display="none"
+    // }
 });
 document.querySelector("#contact").addEventListener("click", function() {
     contactTab();
-    if (hamburger.style.display="block") {
-        navList.style.display="none"
-    }
+    // if (hamburger.style.display="block") {
+    //     navList.style.display="none"
+    // }
 });
 
 
