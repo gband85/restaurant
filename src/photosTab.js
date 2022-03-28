8fbf089576ae04847c57915876e8b307d6de5357
gband85 <garrett@gband.net>
2022-03-28 16:30:27 -0500

    resize photos

--- src/photosTab.js
+++ src/photosTab.js
@@ -8,19 +8,20 @@ const photosTab = () => {
     
     const photosSection=document.createElement("section");
     photosSection.className="photos";
-    photosSection.innerHTML=`<img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-pixabay-262918.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-pixabay-262978.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-roman-odintsov-4552128.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" className="photo" width="200px" height="200px">
-    <img src="../src/pexels-pixabay-460537.jpg" alt="" className="photo" width="200px" height="200px">`
-
+    photosSection.innerHTML=`
+    <img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-helena-lopes-1015568.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-pixabay-262978.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-helena-lopes-693269.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-lisa-fotios-1126728.jpg" alt="" className="photo" width="400" height="270">
+    `
     const container = document.createElement("div");
     container.className="container";
     container.appendChild(title);
