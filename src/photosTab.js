26427327ac0b15eda44d97837af82af42b2f0681
gband85 <garrett@gband.net>
2022-03-27 21:08:43 -0500

    add width and height attributes to photos

--- src/photosTab.js
+++ src/photosTab.js
@@ -8,18 +8,18 @@ const photosTab = () => {
     
     const photosSection=document.createElement("section");
     photosSection.className="photos";
-    photosSection.innerHTML=`<img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" className="photo">
-    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" className="photo">
-    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" className="photo">
-    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" className="photo">
-    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" className="photo">
-    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" className="photo">
-    <img src="../src/pexels-pixabay-262918.jpg" alt="" className="photo">
-    <img src="../src/pexels-pixabay-262978.jpg" alt="" className="photo">
-    <img src="../src/pexels-roman-odintsov-4552128.jpg" alt="" className="photo">
-    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" className="photo">
-    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" className="photo">
-    <img src="../src/pexels-pixabay-460537.jpg" alt="" className="photo">`
+    photosSection.innerHTML=`<img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-pixabay-262918.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-pixabay-262978.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-roman-odintsov-4552128.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" className="photo" width="200px" height="200px">
+    <img src="../src/pexels-pixabay-460537.jpg" alt="" className="photo" width="200px" height="200px">`
 
     const container = document.createElement("div");
     container.className="container";
