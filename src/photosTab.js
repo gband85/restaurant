5496aea34aa522806fa08d44e5820759be5d148e
gband85 <garrett@gband.net>
2022-03-27 20:56:55 -0500

    finish photosTab
    append page elements to container
    append container to main
    export module

--- src/photosTab.js
+++ src/photosTab.js
@@ -6,9 +6,9 @@ const photosTab = () => {
     titleText.textContent="Photos"
     title.appendChild(titleText);
     
-    const photos=document.createElement("section");
-    photos.className="photos";
-    photos.innerHTML=`<img src="./pexels-andrea-piacquadio-821054.jpg" alt="" className="photo">
+    const photosSection=document.createElement("section");
+    photosSection.className="photos";
+    photosSection.innerHTML=`<img src="./pexels-andrea-piacquadio-821054.jpg" alt="" className="photo">
     <img src="./pexels-edward-eyer-687824.jpg" alt="" className="photo">
     <img src="./pexels-helena-lopes-696218.jpg" alt="" className="photo">
     <img src="./pexels-live-on-shot-2788792.jpg" alt="" className="photo">
@@ -20,4 +20,13 @@ const photosTab = () => {
     <img src="./pexels-terje-sollie-299347.jpg" alt="" className="photo">
     <img src="./pexels-valeria-boltneva-1484516.jpg" alt="" className="photo">
     <img src="./pexels-pixabay-460537.jpg" alt="" className="photo">`
+
+    const container = document.createElement("div");
+    container.className="container";
+    container.appendChild(title);
+    container.appendChild(photosSection);
+        main.appendChild(container)
+        content.appendChild(main);
 }
+
+export { photosTab }
