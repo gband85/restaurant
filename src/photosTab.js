01e1d63f5bcb7d2617756a2ba4c008f02cbf39d1
gband85 <garrett@gband.net>
2022-03-29 12:32:55 -0500

    fix img links
    change 'className' to 'class'
    remove dimensions

--- src/photosTab.js
+++ src/photosTab.js
@@ -13,18 +13,18 @@ const photosTab = () => {
     const photosSection=document.createElement("section");
     photosSection.className="photos";
     photosSection.innerHTML=`
-    <img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-helena-lopes-1015568.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-pixabay-262978.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-helena-lopes-693269.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" className="photo" width="400" height="270">
-    <img src="../src/pexels-lisa-fotios-1126728.jpg" alt="" className="photo" width="400" height="270">
+    <img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" class="photo">
+    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" class="photo">
+    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" class="photo">
+    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" class="photo">
+    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" class="photo">
+    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" class="photo">
+    <img src="../src/pexels-helena-lopes-1015568.jpg" alt="" class="photo">
+    <img src="../src/pexels-pixabay-262978.jpg" alt="" class="photo">
+    <img src="../src/pexels-helena-lopes-693269.jpg" alt="" class="photo">
+    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" class="photo">
+    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" class="photo">
+    <img src="../src/pexels-lisa-fotios-1126728.jpg" alt="" class="photo">
     `
     const container = document.createElement("div");
     container.className="container";
