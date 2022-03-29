38e37a428c614d91251a176bfb01606e8f30b692
gband85 <garrett@gband.net>
2022-03-29 16:59:24 -0500

    add menu-item class

--- src/menuTab.js
+++ src/menuTab.js
@@ -16,15 +16,15 @@ const menuTab = () => {
     burgerSection.className="menu-section";
     burgerSection.innerHTML=`
     <p class="menu-title">BURGERS</p>
-    <p>CLASSIC / 25</p>
+    <p class="menu-item">CLASSIC / 25</p>
     <p>A beef patty with caramelized onions,
     pickles, crispy lettuce, and juicy
     tomatoes.</p>
-    <p>DOUBLE / 35</p>
+    <p class="menu-item">DOUBLE / 35</p>
     <p>Two quarter-pounder patties stuffed
     between potato buns, guaranteed to
     fill you up.</p>
-    <p>DAREDEVIL / 40</p>
+    <p class="menu-item">DAREDEVIL / 40</p>
     <p>Three quarter-pounder patties stuffed
     between potato buns. Can you handle
     this?</p>
@@ -33,13 +33,13 @@ const menuTab = () => {
     riceSection.className="menu-section";
     riceSection.innerHTML=`
         <p class="menu-title">RICE MEALS</p>
-        <p>PORK STEAK / 20</p>
+        <p class="menu-item">PORK STEAK / 20</p>
         <p>Pork steak served with rice and a side
         of mashed potatoes, and a salad.</p>
-        <p>CORDON BLEU / 20</p>
+        <p class="menu-item">CORDON BLEU / 20</p>
         <p>With creamy cheese, served with rice
         and mashed potatoes.</p>
-        <p>STEAK / 30</p>
+        <p class="menu-item">STEAK / 30</p>
         <p>A 20 oz. steak, medium rare, served
         with mashed potatoes and mixed
         vegetables.</p>
@@ -48,13 +48,13 @@ const menuTab = () => {
         sandwichSection.className="menu-section";
         sandwichSection.innerHTML=`
         <p class="menu-title">SANDWICHES</p>
-        <p>VEGAN PANINI / 15</p>
+        <p class="menu-item">VEGAN PANINI / 15</p>
         <p>Roasted vegetables, in ciabatta,
         slathered with creamy hummus.</p>
-        <p>ITALIAN SAUSAGE / 20<p>
+        <p class="menu-item">ITALIAN SAUSAGE / 20<p>
         <p>Grilled Italian sausage with pickles,
         tomatoes, and mustard, with a relish.</p>
-        <p>3-CHEESE / 20</p>
+        <p class="menu-item">3-CHEESE / 20</p>
         <p>Swiss, chepar, and mozzarella, in a
         grilled sandwich slathered with
         butter.</p>`
@@ -62,13 +62,13 @@ const menuTab = () => {
         drinkSection.className="menu-section";
         drinkSection.innerHTML=`
         <p class="menu-title">DRINKS</p>
-        <p>JUICE / 5 PER GLASS</p>
+        <p class="menu-item">JUICE / 5 PER GLASS</p>
         <p>Choice of orange, lemon, watermelon,
         mango, grape, or strawberry.</p>
-        <p>ICED COFFEE / 10 PER GLASS</p>
+        <p class="menu-item">ICED COFFEE / 10 PER GLASS</p>
         <p>Mixed with milk, and served over ice
         and topped with chocolate syrup.</p>
-        <p>SHAKE / 15</p>
+        <p class="menu-item">SHAKE / 15</p>
         <p>Watermelon, mango, strawberry-banana, mixed berries, or citrus.</p>`
        // content.innerHTML="";
         //const main = document.querySelector("main");
