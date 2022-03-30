ac5e3cfa2635fd21b8a0417ae946ab82fd9ae07a
gband85 <garrett@gband.net>
2022-03-29 20:44:44 -0500

    move images to separate folder

--- src/photosTab.js
+++ src/photosTab.js
@@ -13,18 +13,18 @@ const photosTab = () => {
     const photosSection=document.createElement("div");
     photosSection.className="photos";
     photosSection.innerHTML=`
-    <img src="../src/pexels-andrea-piacquadio-821054.jpg" alt="" class="photo">
-    <img src="../src/pexels-edward-eyer-687824.jpg" alt="" class="photo">
-    <img src="../src/pexels-helena-lopes-696218.jpg" alt="" class="photo">
-    <img src="../src/pexels-live-on-shot-2788792.jpg" alt="" class="photo">
-    <img src="../src/pexels-mali-maeder-64208.jpg" alt="" class="photo">
-    <img src="../src/pexels-picjumbocom-225228.jpg" alt="" class="photo">
-    <img src="../src/pexels-helena-lopes-1015568.jpg" alt="" class="photo">
-    <img src="../src/pexels-pixabay-262978.jpg" alt="" class="photo">
-    <img src="../src/pexels-helena-lopes-693269.jpg" alt="" class="photo">
-    <img src="../src/pexels-terje-sollie-299347.jpg" alt="" class="photo">
-    <img src="../src/pexels-valeria-boltneva-1484516.jpg" alt="" class="photo">
-    <img src="../src/pexels-lisa-fotios-1126728.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-andrea-piacquadio-821054.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-edward-eyer-687824.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-helena-lopes-696218.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-live-on-shot-2788792.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-mali-maeder-64208.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-picjumbocom-225228.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-helena-lopes-1015568.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-pixabay-262978.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-helena-lopes-693269.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-terje-sollie-299347.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-valeria-boltneva-1484516.jpg" alt="" class="photo">
+    <img src="../src/images/pexels-lisa-fotios-1126728.jpg" alt="" class="photo">
     `
     const container = document.createElement("div");
     container.className="container";
