9a75840629fbe134b7a86f479d4f2f2943d4aa06
gband85 <garrett@gband.net>
2022-04-07 21:09:25 -0500

    switch link to btn class

--- src/headerModule.js
+++ src/headerModule.js
@@ -5,11 +5,11 @@ const headerModule = () => {
   header.innerHTML=`<nav class="navbar">
   <a href="index.html" class="logo">RSR</a>
   <ul class="nav-list">
-      <li id="home"><button class="link">Home</button></li>
-      <li id="menu"><button class="link">Menu</button></li>
-      <li id="reviews"><button class="link">Reviews</button></li>
-      <li id="photos"><button class="link">Photos</button></li>
-      <li id="contact"><button class="link">Contact</button></li>
+      <li id="home"><button class="btn">Home</button></li>
+      <li id="menu"><button class="btn">Menu</button></li>
+      <li id="reviews"><button class="btn">Reviews</button></li>
+      <li id="photos"><button class="btn">Photos</button></li>
+      <li id="contact"><button class="btn">Contact</button></li>
   </ul>
   <i id="hamburger" class="fa-solid fa-bars"></i>
 </nav>`
