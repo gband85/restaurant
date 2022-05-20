901e90404bedc98fd709734c5cd4dad9a9973402
gband85 <garrett@gband.net>
2022-05-20 16:53:34 -0500

    adjust element order

--- src/headerModule.js
+++ src/headerModule.js
@@ -2,17 +2,16 @@ const headerModule = () => {
   //create header
   const header = document.createElement("header");
   header.id = "header";
-  header.innerHTML=`<a href="index.html" class="logo">RSR</a>
-  <nav class="navbar">
-  
-  <ul class="nav-list">
+  header.innerHTML=`<nav class="navbar">
+  <a href="index.html" class="logo">RSR</a>
+     <ul class="nav-list" id="nav-list">
       <li id="home"><button class="btn">Home</button></li>
       <li id="menu"><button class="btn">Menu</button></li>
       <li id="reviews"><button class="btn">Reviews</button></li>
       <li id="photos"><button class="btn">Photos</button></li>
       <li id="contact"><button class="btn">Contact</button></li>
   </ul>
-  <i id="hamburger" class="fa-solid fa-bars"></i>
+ <i id="hamburger" class="fa-solid fa-bars"></i>
 </nav>`
   
   return header;
