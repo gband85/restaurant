86e6b50d14cde49db7e86ce905b176e669eb64c4
gband85 <garrett@gband.net>
2022-05-24 10:39:35 -0500

    add nav-button div

--- src/headerModule.js
+++ src/headerModule.js
@@ -3,7 +3,10 @@ const headerModule = () => {
   const header = document.createElement("header");
   header.id = "header";
   header.innerHTML=`<nav class="navbar">
+<div id="nav-buttons">
   <a href="index.html" class="logo">RSR</a>
+  <i id="hamburger" class="fa-solid fa-bars"></i>
+  </div>
      <ul class="nav-list" id="nav-list">
       <li id="home"><button class="btn">Home</button></li>
       <li id="menu"><button class="btn">Menu</button></li>
@@ -11,7 +14,7 @@ const headerModule = () => {
       <li id="photos"><button class="btn">Photos</button></li>
       <li id="contact"><button class="btn">Contact</button></li>
   </ul>
- <i id="hamburger" class="fa-solid fa-bars"></i>
+ 
 </nav>`
   
   return header;
